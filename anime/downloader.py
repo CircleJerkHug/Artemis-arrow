67f885fcef9375b8e8b435a491f696537822c0cf
CircleJerkHug <devansh.prakhar@gmail.com>
2018-12-19 15:33:05 +0530

    Opened the page after seaching

--- anime/downloader.py
+++ anime/downloader.py
@@ -1,12 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys
 
-url="https://kissanime.ru/Search/Anime/"
-'''
-print("Enter the name of anime\n")
-name=input()
-url_new=url+str(name)
-print(url_new)
-'''
-r=requests.get(url)
-print(r)
+driver=webdriver.Firefox()
+driver.get("https://kissanime.ru/")
+driver.maximize_window()
+
+
+
+search = WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.ID, "keyword"))).click()
+search=driver.find_element_by_xpath('//*[@id="keyword"]')
+search.send_keys("boruto")
+search.send_keys(Keys.RETURN)
